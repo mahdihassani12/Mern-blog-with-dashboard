7d330e70ce0371a9f0e03a4ac51e13c4b2f5f993
Mahdi Hassani <hassanim430@gmail.com>
2024-08-25 17:40:53 +0430

    connecting to database

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
+import connectDB from "./utils/db.js";
+connectDB();
+
 const port = process.env.PORT || 4050;
 const app = express();
 
