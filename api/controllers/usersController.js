26a69a25d8d946671b82133c9d666cce4d77693f
Mahdi Hassani <hassanim430@gmail.com>
2024-08-25 17:48:19 +0430

    adding register function

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -3,7 +3,51 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
 // adding user to database
-const registerUser = async (req, res, next) => {};
+const registerUser = async (req, res, next) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    // Validate required fields
+    if (!name || !email || !password || !role) {
+      res.status(400);
+      throw new Error("Please add all fields");
+    }
+
+    // Check if user exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400);
+      throw new Error("User already exists");
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    // Send success response
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid user data.");
+    }
+  } catch (error) {
+    next(error); // Pass the error to the error handler
+  }
+};
 
 // authenticate a user on login
 const loginUser = async (req, res, next) => {};
