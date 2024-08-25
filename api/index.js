3e6353b11af8874ad68aceed7ad63266abde567c
Mahdi Hassani <hassanim430@gmail.com>
2024-08-25 17:36:45 +0430

    initialize the routes

--- api/index.js
+++ api/index.js
@@ -8,4 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// initialize the routes
+import users from "./routes/users.js";
+app.use("/api/users", users);
+
 app.listen(port, () => console.log(`Server is runing on port ${port}`));
