8bed149d22201dfe9cd63599654670fcf9e2b061
Mahdi Hassani <hassanim430@gmail.com>
2024-08-25 18:14:25 +0430

    adding jwt auth to authentications

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -39,6 +39,7 @@ const registerUser = async (req, res, next) => {
         name: user.name,
         email: user.email,
         role: user.role,
+        token: generateToken(user._id),
       });
     } else {
       res.status(400);
@@ -50,12 +51,43 @@ const registerUser = async (req, res, next) => {
 };
 
 // authenticate a user on login
-const loginUser = async (req, res, next) => {};
+const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    // check for user email
+    const user = await User.findOne({ email });
+
+    // check for matching password
+    const checkPassword = await bcrypt.compare(password, user.password);
+
+    if (user && checkPassword) {
+      res.status(200).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid user data.");
+    }
+  } catch (error) {
+    next(error); // Pass the error to the error handler
+  }
+};
 
 // authenticate a user on validation
-const getMe = async (req, res, next) => {};
+const getMe = async (req, res, next) => {
+  res.status(200).json(req.user);
+};
 
 // Generate jwt
-const generateToken = (id) => {};
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 export { registerUser, loginUser, getMe };
--- api/routes/users.js
+++ api/routes/users.js
@@ -7,8 +7,10 @@ import {
   getMe,
 } from "../controllers/usersController.js";
 
+import protect from "../middleware/auth.js";
+
 router.post("/", registerUser);
 router.post("/login", loginUser);
-router.get("/me", getMe);
+router.get("/me", protect, getMe);
 
 export default router;
