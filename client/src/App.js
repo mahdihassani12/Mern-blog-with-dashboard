1e746b2a1e4dab6dbdde0627ac971cc992f26022
Mahdi Hassani <hassanim430@gmail.com>
2024-08-29 14:21:10 +0430

    adding basic react router configurations

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <div>
+        <Outlet />
+      </div>
+    </>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
+import Home from "./pages/frontend/Home";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App></App>}>
+        <Route path="/" element={<Home />} /> {/* /frontend home page */}
+        <Route path="/dashboard" element={<Dashboard />} /> {/* /dashboard home page */}
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
